69d109facabccf16c59e2e5e3d993abaf5441ab3
trudwick <trudwick@users.noreply.github.com>
2023-04-26 12:06:07 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -193,8 +193,8 @@ function buildDisp(){
   let ct = 0;
   dispStrs[2]=""
   dispStrs[3] = ""
-  dispStrs[4] = ""
-  dispStrs[5] = ""
+  dispStrs[4] = "Note: All operations are left to right, ignoring OOP. That is, assume all OOP are the same priority<br>"
+  dispStrs[5] = "Note: All operations are left to right, ignoring OOP. That is, assume all OOP are the same priority<br>"
   for (let k in fullSolutions) {
     // console.log('Hi',k,fullSolutions[k],Object.keys(fullSolutions[k]).length)
     ct+=Object.keys(fullSolutions[k]).length;
